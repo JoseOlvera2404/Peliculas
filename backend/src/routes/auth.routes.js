946249dc5418cd8a790b996af82fd1e3fb08c799
Jose Luis Olvera Garcia <joseolveragarcia24@gmail.com>
2026-02-21 21:27:37 -0600

    Bugfix: Cambio de import a const

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
-import authMiddleware from '../middleware/auth.middleware';
+const authMiddleware = require('../middleware/auth.middleware');
+const rolMiddleware = require('../middleware/rol.middleware');
 
 /**
  * @swagger
